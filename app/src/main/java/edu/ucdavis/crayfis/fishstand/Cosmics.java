1a2dcbdf3fdaf74b12ef8f592e68c148f8800915
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2018-08-14 09:57:27 -0700

    Implemented more features for Cosmics

--- app/src/main/java/edu/ucdavis/crayfis/fishstand/Cosmics.java
+++ app/src/main/java/edu/ucdavis/crayfis/fishstand/Cosmics.java
@@ -6,53 +6,63 @@ import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.Type;
 import android.util.Log;
+import android.util.Pair;
 
 import java.io.DataOutputStream;
 import java.io.OutputStream;
 import java.nio.ShortBuffer;
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Cosmics implements Analysis {
     private static final String TAG = "Cosmics";
 
     //private static final long FILE_SIZE = 5000000L;
-    private static final int FILE_COUNT = 0;
+    private static final int FILE_COUNT = 1;
 
     private AtomicInteger images = new AtomicInteger();
 
     private Allocation abuf;
     private Allocation ahist;
-    private static char max_hist = 1023;
+    private static final int MAX_HIST = App.getConfig().getBoolean("yuv", false) ? 255 : 1023;
     private Allocation ax;
     private Allocation ay;
     private Allocation aval;
     private Allocation an;
-    private static final int MAX_N = 120;
+    private static final int MAX_N = 50;
+    private Allocation aweights;
     private ScriptC_cosmics script;
 
-    private int THRESH = 1000;
+    private int[] pix_x_evt;
+    private int[] pix_y_evt;
+    private int[] pix_val_evt;
+
+    private ArrayList<Short> pixX = new ArrayList<>();
+    private ArrayList<Short> pixY = new ArrayList<>();
+    private ArrayList<Short> pixVal = new ArrayList<>();
+
+    // TODO: how do we populate this list?
+    private ArrayList<Pair<Integer, Integer>> hotXY = new ArrayList<>();
+
+    private static final double PASS_RATE = .1;
+    private static int thresh = MAX_HIST;
 
     public Cosmics() {
 
 	    int nx = App.getCamera().getResX();
         int ny = App.getCamera().getResY();
-        int num_pixels = nx * ny;
-
-        App.log().append("num_pixels:   " + num_pixels + "\n");
 
         RenderScript rs = App.getRenderScript();
         script = new ScriptC_cosmics(rs);
 
-        Type type16 = new Type.Builder(rs, Element.U16(rs))
+        abuf = Allocation.createTyped(rs, new Type.Builder(rs, Element.U16(rs))
                 .setX(nx)
                 .setY(ny)
-                .create();
-
-        abuf = Allocation.createTyped(rs, type16);
-        ahist = Allocation.createSized(rs, Element.U64(rs), max_hist+1, Allocation.USAGE_SCRIPT);
+                .create());
+        ahist = Allocation.createSized(rs, Element.U64(rs), MAX_HIST+1, Allocation.USAGE_SCRIPT);
         ax = Allocation.createSized(rs, Element.U32(rs), MAX_N, Allocation.USAGE_SCRIPT);
         ay = Allocation.createSized(rs, Element.U32(rs), MAX_N, Allocation.USAGE_SCRIPT);
-        aval = Allocation.createSized(rs, Element.U16(rs), MAX_N, Allocation.USAGE_SCRIPT);
+        aval = Allocation.createSized(rs, Element.U32(rs), MAX_N, Allocation.USAGE_SCRIPT);
         an = Allocation.createSized(rs, Element.U32(rs), 1, Allocation.USAGE_SCRIPT);
 
         script.bind_gHist(ahist);
@@ -61,7 +71,26 @@ public class Cosmics implements Analysis {
         script.bind_gPixVal(aval);
         script.bind_gPixN(an);
         script.set_gMaxN(MAX_N);
-        script.set_gThresh(THRESH);
+        script.set_gThresh(thresh);
+
+        //hotXY.add(new Pair<>(1243, 924));
+        //hotXY.add(new Pair<>(1243, 923));
+
+        if(hotXY.size() > 0) {
+            aweights = Allocation.createTyped(rs, new Type.Builder(rs, Element.F16(rs))
+                    .setX(nx)
+                    .setY(ny)
+                    .create());
+
+            script.forEach_setToUnity(aweights, aweights);
+            for(int i=0; i<hotXY.size(); i++) {
+                Pair<Integer, Integer> xy = hotXY.get(i);
+                aweights.copy2DRangeFrom(xy.first, xy.second, 1, 1,
+                        new short[]{Float.valueOf(0f).shortValue()});
+            }
+        }
+
+        App.log().append("thresh: " + thresh + "\n");
     }
 
     public void ProcessImage(Image img) {
@@ -79,25 +108,35 @@ public class Cosmics implements Analysis {
             buf.rewind();
         }
 
+        final int pixN;
         synchronized (abuf) {
             abuf.copyFromUnchecked(vals);
-            script.forEach_histogram(abuf);
-            int[] pixN = new int[1];
-            an.copyTo(pixN);
-            Log.d(TAG, "pixN = " + pixN[0]);
-            if(pixN[0] > 0) {
-                int[] pixX = new int[pixN[0]];
-                int[] pixY = new int[pixN[0]];
-                int[] pixVal = new int[pixN[0]];
-                ax.copy1DRangeToUnchecked(0, pixN[0], pixX);
-                ay.copy1DRangeToUnchecked(0, pixN[0], pixY);
-                aval.copy1DRangeToUnchecked(0, pixN[0], pixVal);
-
-                for(int i=0; i<pixN[0]; i++) {
-                    App.log().append("(" + pixX[i] + ", " + pixY[i] + "): " + pixVal[i] + "\n");
-                }
-                script.invoke_reset();
+            if(aweights == null) {
+                script.forEach_histogram(abuf);
+            } else {
+                script.forEach_weighted_histogram(abuf, aweights);
             }
+            final int[] pixNcontainer = new int[1];
+            an.copyTo(pixNcontainer);
+            pixN = Math.min(pixNcontainer[0], MAX_N);
+            script.invoke_reset();
+
+            if(pixN > 0) {
+                pix_x_evt = new int[pixN];
+                pix_y_evt = new int[pixN];
+                pix_val_evt = new int[pixN];
+                ax.copy1DRangeToUnchecked(0, pixN, pix_x_evt);
+                ay.copy1DRangeToUnchecked(0, pixN, pix_y_evt);
+                aval.copy1DRangeToUnchecked(0, pixN, pix_val_evt);
+            }
+        }
+
+        // TODO: should we add nearby pixels too?
+        for(int i=0; i<pixN; i++) {
+            pixX.add((short)pix_x_evt[i]);
+            pixY.add((short)pix_y_evt[i]);
+            pixVal.add((short)pix_val_evt[i]);
+            Log.d(TAG,"(" + pix_x_evt[i] + ", " + pix_y_evt[i] + "): " + pix_val_evt[i] + "\n");
         }
     }
 
@@ -110,21 +149,30 @@ public class Cosmics implements Analysis {
         long[] hist_array = new long[ahist.getBytesSize()/8];
         ahist.copyTo(hist_array);
 
-        int total_pix = 0;
-        int sum = 0;
-        for(int i=0; i<hist_array.length; i++) {
-            total_pix += hist_array[i];
+        long sum = 0;
+        long pix = 0;
+        int target_pix = (int) (PASS_RATE*images.intValue());
+        Log.d(TAG, "target = " + target_pix);
+
+        boolean new_thresh = false;
+        for(int i=hist_array.length-1; i>=0; i--) {
             sum += hist_array[i] * i;
-            //Log.d(TAG, "hist[" + i + "] = " + hist_array[i]);
+            pix += hist_array[i];
+            if (pix > target_pix && !new_thresh) {
+                thresh = i;
+                new_thresh = true;
+            }
+            Log.d(TAG, "hist[" + i + "] = " + hist_array[i] + " total = " + pix);
         }
-        App.log().append("Mean: " + sum / (double)total_pix + "\n");
+        App.log().append("Mean: " + sum / (double)pix + "\n");
 
         if(FILE_COUNT > 0) {
-            WriteOutput(hist_array, images.intValue());
+            WriteOutput(hist_array, images.intValue(), pixX, pixY, pixVal);
         }
     }
 
-    private static void WriteOutput(final long[] hist, int num_images){
+    private static void WriteOutput(final long[] hist, int num_images,
+                                    ArrayList<Short> pixX, ArrayList<Short> pixY, ArrayList<Short> pixVal){
         try {
             int run_num = App.getPref().getInt("run_num", 0);
 
@@ -132,7 +180,7 @@ public class Cosmics implements Analysis {
             final int HEADER_SIZE = 7;
             final int VERSION = 1;
 
-            String filename = "run_" + run_num + "_cosmics_summary.dat";
+            String filename = "run_" + run_num + "_cosmics_hist.dat";
             App.log().append("writing file " + filename + "\n");
             OutputStream out = Storage.newOutput(filename);
             DataOutputStream writer = new DataOutputStream(out);
@@ -149,9 +197,39 @@ public class Cosmics implements Analysis {
             for (long bin: hist) {
                 writer.writeLong(bin);
             }
+
             writer.flush();
             writer.close();
             out.close();
+
+            if(pixX.size() > 0) {
+                // now write the events
+                filename = "run_" + run_num + "_cosmics_events.dat";
+                App.log().append("writing file " + filename + "\n");
+                out = Storage.newOutput(filename);
+                writer = new DataOutputStream(out);
+                writer.writeInt(HEADER_SIZE);
+                writer.writeInt(VERSION);
+                //additional header items (should match above size!)
+                writer.writeInt(num_images);
+                writer.writeInt(FILE_COUNT);
+                writer.writeInt(App.getCamera().getResX());
+                writer.writeInt(App.getCamera().getResY());
+                writer.writeInt(App.getCamera().getISO());
+                writer.writeInt((int) App.getCamera().getExposure());
+                writer.writeInt(hist.length);
+
+                for (int i = 0; i < pixX.size(); i++) {
+                    writer.writeShort(pixX.get(i));
+                    writer.writeShort(pixY.get(i));
+                    writer.writeShort(pixVal.get(i));
+                }
+
+                writer.flush();
+                writer.close();
+                out.close();
+            }
+
         } catch(Exception e) {
             Log.e(TAG, e.getMessage());
             Log.e(TAG, "Failed to save results.");
--- app/src/main/rs/cosmics.rs
+++ app/src/main/rs/cosmics.rs
@@ -4,24 +4,43 @@
 
 uint64_t* gHist;
 ushort gThresh;
-uint32_t* gPixX;
+uint32_t* gPixX; // these need to be 32-bit, otherwise the addresses get complicated
 uint32_t* gPixY;
-ushort* gPixVal;
+uint32_t* gPixVal;
 uint32_t* gPixN;
 uint gMaxN;
+
+
+void RS_KERNEL histogram(ushort raw, uint32_t x, uint32_t y) {
+    volatile uint32_t* addr = &gHist[raw];
+    rsAtomicInc(addr);
+
+    if (raw > gThresh && *gPixN < gMaxN) {
+        uint pixN = rsAtomicInc(gPixN);
+        *(gPixX+pixN) = x;
+        *(gPixY+pixN) = y;
+        *(gPixVal+pixN) = raw;
+    }
+}
+
+void RS_KERNEL weighted_histogram(ushort raw, half weight, uint32_t x, uint32_t y) {
+    ushort adjusted = (ushort) (raw * weight);
+
+    volatile uint32_t* addr = &gHist[adjusted];
+    rsAtomicInc(addr);
+
+    if (adjusted > gThresh && *gPixN < gMaxN) {
+        uint pixN = rsAtomicInc(gPixN);
+        *(gPixX+pixN) = x;
+        *(gPixY+pixN) = y;
+        *(gPixVal+pixN) = raw;
+    }
+}
 
 void reset() {
     *gPixN = 0;
 }
 
-void RS_KERNEL histogram(ushort in, uint32_t x, uint32_t y) {
-    volatile uint32_t* addr = &gHist[in];
-    rsAtomicInc(addr);
-
-    if (in > gThresh && *gPixN < gMaxN) {
-        uint pixN = rsAtomicInc(gPixN);
-        *(gPixX+pixN) = x;
-        *(gPixY+pixN) = y;
-        *(gPixVal+pixN) = in;
-    }
+half RS_KERNEL setToUnity(half in) {
+    return 1;
 }
